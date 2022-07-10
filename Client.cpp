a318e60db334b89756cdfdec2fa672411c829321
himnegi1 <108433012+himnegi1@users.noreply.github.com>
2022-07-10 20:07:31 +0530

    Update Client.cpp

--- Client.cpp
+++ Client.cpp
@@ -3,9 +3,10 @@
 int main()
 {
 	const string name = "MainFunction";
-	
+	//first initialize loggerClass 
 	LoggerInitializer();
 	
+	//Below calls can be from different thread too
 	LOG_TRACE("Trace :", name);
 	LOG_DEBUG("Debug :", name);
 	LOG_INFO("Info :", name);
